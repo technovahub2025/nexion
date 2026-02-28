ac53880e5d276b759476bdb6883f795ec05d8bed
Technovahub Career <technovahubcareer@gmail.com>
2026-02-28 18:27:40 +0530

    number_added

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -19,7 +19,7 @@ export const Contact = () => {
 
             <div>
               <span>Phone</span>
-              <p>+91 9XXXXXXXXX</p>
+              <p>+91 +91 9629600230</p>
             </div>
           </div>
 
